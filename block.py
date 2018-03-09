7ff118e6298d606b80ed94f8fde23db48fc97a65
Tucker Massad <massadl@union.edu>
2018-03-09 18:01:02 -0500

    further block updates

--- block.py
+++ block.py
@@ -3,7 +3,7 @@ import random, binascii
 class Block:
     def __init__(self):
         self.nonce = self.generateNonce();
-#        self.parentNode;
+        self.parentNode = None;
 
 
     def generateNonce(self):
@@ -11,6 +11,10 @@ class Block:
 
     def print(self):
         print(self.nonce)
+    
+    #returns True if hash is verified to be true, false otherwise
+    def verify(self):
+        return
 
 testblock = Block()
 testblock.generateNonce()
